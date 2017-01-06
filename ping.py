ddec5bcec40976cc11a8a33ab1e86793975d6971
Sijis Aviles <sijis.aviles+github@gmail.com>
2017-01-05 22:31:50 -0600

    Convert to newer errbot api

--- ping.py
+++ ping.py
@@ -6,8 +6,4 @@ class Ping(BotPlugin):
     @botcmd
     def ping(self, msg, args):
         """ this command responds 'pong' """
-        self.send(msg.frm,
-                  'pong',
-                  message_type=msg.type,
-                  in_reply_to=msg,
-                  groupchat_nick_reply=True)
+        return 'pong'
